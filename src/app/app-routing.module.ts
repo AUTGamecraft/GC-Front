a67697435d7449e041d6bd7f991b4e82a1108c1b
ammirrezabsh <amirrezabsh79@gmail.com>
2021-01-15 12:45:45 +0330

    home component and header component modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
-  {path: 'signup',component:SignUpPageComponent}
+  {path: 'signup',component:SignUpPageComponent},
+  {path: 'home',component:HomeComponent}
 ];
 
 @NgModule({
