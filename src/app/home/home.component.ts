7dba973a6212b44e8ac1dd29fe666836e9f175d6
ammirrezabsh <amirrezabsh79@gmail.com>
2021-02-07 21:36:11 +0330

    timer done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,42 +9,42 @@ import * as moment from 'jalali-moment';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  workshopsArray : any = {};
-  workshopsActive : any = {};
-  iconW : any = {};
-  workshopsDate : any = {};
-  talksArray : any = {};
-  talksActive : any = {};
-  iconT : any = {};
-  talksDate : any = {};
-  m : string = '' ;
-  constructor(public publicservice:PublicService,public router:Router) { 
-    publicservice.getTalks().then((r) =>{
+  workshopsArray: any = {};
+  workshopsActive: any = {};
+  iconW: any = {};
+  workshopsDate: any = {};
+  talksArray: any = {};
+  talksActive: any = {};
+  iconT: any = {};
+  talksDate: any = {};
+  m: string = '';
+  time = 1000;
+  
+  constructor(public publicservice: PublicService, public router: Router) {
+    publicservice.getTalks().then((r) => {
       this.talksArray = r.data;
-      for (let index = 0; index < this.talksArray.length;index++) {
+      for (let index = 0; index < this.talksArray.length; index++) {
         this.talksActive[index] = 'deactive'
         this.iconT[index] = 'keyboard_arrow_down'
-        this.m =moment(this.talksArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('YYYY-MM-DD');
-        this.talksDate[index] =moment(this.talksArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('dddd') + " " + moment(this.talksArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('DD') + " " + moment(this.talksArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('MMMM') + " " + moment(this.talksArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('YY');
+        this.m = moment(this.talksArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('YYYY-MM-DD');
+        this.talksDate[index] = moment(this.talksArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('dddd') + " " + moment(this.talksArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('DD') + " " + moment(this.talksArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('MMMM') + " " + moment(this.talksArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('YY');
       }
     })
-    publicservice.getWorkshops().then((r) =>{
+    publicservice.getWorkshops().then((r) => {
       this.workshopsArray = r.data;
       console.log(r)
-      for (let index = 0; index < this.workshopsArray.length;index++) {
+      for (let index = 0; index < this.workshopsArray.length; index++) {
         this.workshopsActive[index] = 'deactive'
         this.iconW[index] = 'keyboard_arrow_down'
-        this.m =moment(this.workshopsArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('YYYY-MM-DD');
-        this.workshopsDate[index] =moment(this.workshopsArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('dddd') + " " + moment(this.workshopsArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('DD') + " " + moment(this.workshopsArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('MMMM') + " " + moment(this.workshopsArray[index].date.split('T',2)[0],'YYYY-MM-DD').locale('fa').format('YY');
+        this.m = moment(this.workshopsArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('YYYY-MM-DD');
+        this.workshopsDate[index] = moment(this.workshopsArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('dddd') + " " + moment(this.workshopsArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('DD') + " " + moment(this.workshopsArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('MMMM') + " " + moment(this.workshopsArray[index].date.split('T', 2)[0], 'YYYY-MM-DD').locale('fa').format('YY');
       }
     })
-
-      
-
+    setInterval(()=>this.time = this.time-1,1000);
   }
 
   ngOnInit(): void {
-    
+    this.timer();
   }
   Signup() {
     const navigationDetails2: string[] = ['signup'];
@@ -58,13 +58,16 @@ export class HomeComponent implements OnInit {
     this.publicservice.logedIn = false;
     localStorage.removeItem("Authorization");
   }
-  Dashboard(){
-    this.router.navigate(['dashboard-event']);  
+  Dashboard() {
+    this.router.navigate(['dashboard-event']);
   }
-  Workshop(el: HTMLElement){
-    el.scrollIntoView({behavior: "smooth"});
+  Workshop(el: HTMLElement) {
+    el.scrollIntoView({ behavior: "smooth" });
   }
-  Talk(el: HTMLElement){
-    el.scrollIntoView({behavior: "smooth"});
+  Talk(el: HTMLElement) {
+    el.scrollIntoView({ behavior: "smooth" });
+  }
+  timer(){
+    return  this.time%60+" : "+parseInt(this.time/60+"")+" : "+parseInt(this.time/3600+"");
   }
 }
