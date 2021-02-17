6ec14ea4ebfe5fcbc1207933a5a7ce2508daff56
ammirrezabsh <amirrezabsh79@gmail.com>
2021-02-17 22:05:27 +0330

    query param reader added

--- src/app/confirm-confirm/confirm-confirm.component.ts
+++ src/app/confirm-confirm/confirm-confirm.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit, Query } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PublicService } from '../public.service';
 @Component({
   selector: 'app-confirm-confirm',
@@ -7,12 +7,16 @@ import { PublicService } from '../public.service';
   styleUrls: ['./confirm-confirm.component.scss']
 })
 export class ConfirmConfirmComponent implements OnInit {
-
-  constructor(private router: Router, public publicservice: PublicService) {
+  hash:string = '';
+  constructor(private router: Router, public publicservice: PublicService,private route : ActivatedRoute) {
 
    }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.hash= params['activation'];
+      });
+      console.log(this.hash);
   }
   Dashboard() {
     this.publicservice.ActivateUser('981fbc441cc86f95cdbc858c9e71730e').then((r)=>{
