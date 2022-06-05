708c9ee4a8bc27ad41b6c2bb34712b3ddab9add1
Ashkan Shakiba <AshkanShakiba11@gmail.com>
2022-06-05 18:01:00 +0430

    Making elements responsive

--- src/app/comment/comment.component.ts
+++ src/app/comment/comment.component.ts
@@ -17,9 +17,6 @@ export class CommentComponent implements OnInit {
   @Input() comment: Comment;
   goldenStars: [] = [];
   grayStars: [] = [];
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
-  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
-  originally bred for hunting.`;
 
   constructor() { }
 
